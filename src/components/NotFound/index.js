48e0dce174c7271d496ef96a748e750b74f06bc6
bhavyasri <bhavyasrisonga@gmail.com>
2023-08-24 08:48:44 +0000

    era

---
+++ src/components/NotFound/index.js
@@ -0,0 +1,27 @@
+import {Component} from 'react'
+
+import Header from '../Header'
+
+import './index.css'
+
+class NotFound extends Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <div className="notFound">
+          <div>
+            <img
+              src="https://assets.ccbp.in/frontend/react-js/tech-era/not-found-img.png"
+              alt="not found"
+            />
+          </div>
+          <h1>Page Not Found</h1>
+          <p>We are sorry, the page you requested could not be found.</p>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default NotFound
